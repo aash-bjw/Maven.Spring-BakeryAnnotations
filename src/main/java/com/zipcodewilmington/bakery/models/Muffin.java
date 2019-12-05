7b42b49584d8883cecc2cee9c082894cf28a83e0
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2019-12-05 15:36:37 -0500

    complete

--- src/main/java/com/zipcodewilmington/bakery/models/Muffin.java
+++ src/main/java/com/zipcodewilmington/bakery/models/Muffin.java
@@ -1,8 +1,17 @@
 package com.zipcodewilmington.bakery.models;
 
+import com.sun.javafx.beans.IDProperty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Muffin {
+
+    @Id
+    @GeneratedValue
 
     private Long id;
 
